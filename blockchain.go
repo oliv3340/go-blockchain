5f4fa638a0f990d1b339b8dc9f3c82a1f156b33c
oliv3340 <olivier.graciannette#gmail.com>
2024-04-16 17:38:01 +0200

    add missing err handling

--- blockchain.go
+++ blockchain.go
@@ -58,6 +58,9 @@ func (bc *Blockchain) MineBlock(transactions []*Transaction) {
 
 		return nil
 	})
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 // FindUnspentTransactions returns a list of transactions containing unspent outputs
